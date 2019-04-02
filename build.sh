d4df384cff8411234d42d7633bb501785113d140
SixtyHz <sixtyhz@protonmail.com>
2019-04-02 23:28:53 +0300

    Добавлен код Hello World

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+name="main"
+
+cc65 -Oirs $name.c --add-source
+
+ca65 crt0.s
+ca65 $name.s -g
+
+ld65 -C nrom_32k_vert.cfg -o $name.nes crt0.o $name.o nes.lib -Ln labels.txt
+
+rm *.o
+
+mv labels.txt build
+mv $name.s build
+mv $name.nes build
+
+fceux build/$name.nes
